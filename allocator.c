8d374439d70a0531f3d6e7e9cdccf7fcd6c33f7b
Jason West <jasonwest1013@yahoo.com>
2026-08-12 23:33:52 -0400

    Add coalescing of adjacent memory blocks to my_free
    - my_free looks for free adjacent pre-allocated memory blocks for
      better memory usage
    - Convert free_list to a doubly linked list
    - Add footer to blocks

--- allocator.c
+++ allocator.c
@@ -6,14 +6,49 @@
 
 #define ALIGN_UP(x, align) (((x) + (align) - 1) & ~((uintptr_t)(align) - 1))
 #define HEADER_SIZE ALIGN_UP(sizeof(struct header), ALIGNMENT)
+#define FOOTER_SIZE ALIGN_UP(sizeof(struct footer), ALIGNMENT)
 
 struct header {
   size_t payload_size;
-  bool is_free;
   struct header *next;
+  struct header *prev;
+  bool is_free;
 };
 
+struct footer {
+  size_t payload_size;
+};
+
+static char *starting_break = NULL;
+
 static struct header *free_list_head = NULL;
+
+static void add_head_node(struct header *ptr) {
+  if (free_list_head == NULL) {
+    ptr->next = NULL;
+    ptr->prev = NULL;
+    free_list_head = ptr;
+  } else {
+    ptr->prev = NULL;
+    ptr->next = free_list_head;
+    free_list_head->prev = ptr;
+    free_list_head = ptr;
+  }
+}
+
+static void remove_list_node(struct header *ptr) {
+  if (ptr->next == NULL && ptr->prev == NULL) {
+    free_list_head = NULL;
+  } else if (ptr->prev == NULL) {
+    free_list_head = ptr->next;
+    ptr->next->prev = NULL;
+  } else if (ptr->next == NULL) {
+    ptr->prev->next = NULL;
+  } else {
+    ptr->prev->next = ptr->next;
+    ptr->next->prev = ptr->prev;
+  }
+}
 
 void *my_malloc(size_t size) {
   if (size == 0) {
@@ -23,52 +58,81 @@ void *my_malloc(size_t size) {
   void *current_break = sbrk(0);
 
   uintptr_t addr_padding = (uintptr_t)(-(intptr_t)current_break) & (ALIGNMENT - 1);
+
+  if (starting_break == NULL) {
+    starting_break = (char *)current_break + addr_padding;
+  }
+
   uintptr_t rounded_up_size = ALIGN_UP(size, ALIGNMENT); 
 
-  struct header *prev_header = NULL;
   struct header *current_header = free_list_head;
   while (current_header != NULL) {
     if (current_header->is_free && current_header->payload_size >= rounded_up_size) {
-      
       struct header *found = current_header;
       found->is_free = false;
-
-      if (prev_header == NULL) {
-        free_list_head = current_header->next;
-      } else { 
-        prev_header->next = current_header->next;
-      }
-
+      remove_list_node(found); 
       return (char *)found + HEADER_SIZE;
     }
-    prev_header = current_header;
     current_header = current_header->next;
-
   }
 
-  uintptr_t total_allocation = addr_padding + rounded_up_size + HEADER_SIZE;
+  uintptr_t total_allocation = addr_padding + rounded_up_size + HEADER_SIZE + FOOTER_SIZE;
   void *result = sbrk((intptr_t)total_allocation);
 
   if (result == (void *)-1) {
     return NULL;
-  }
+  } 
 
   struct header *block_header = (struct header *)((char *)current_break + addr_padding);
   block_header->payload_size = rounded_up_size;
   block_header->is_free = false;
   block_header->next = NULL;
+  block_header->prev = NULL;
 
-  return (char *)current_break + addr_padding + HEADER_SIZE;
-}
+  struct footer *block_footer = (struct footer *)((char *)current_break + addr_padding + HEADER_SIZE + rounded_up_size);
+  block_footer->payload_size = rounded_up_size;
 
-void my_free(void *ptr) {
+  return (char *)current_break + addr_padding + HEADER_SIZE; 
+} 
+
+void my_free(void *ptr) { 
   if (ptr == NULL) {
     return;
   }
 
   struct header *freed_header = (struct header *)((char *)ptr - HEADER_SIZE);
   freed_header->is_free = true;
-  freed_header->next = free_list_head;
-  free_list_head = freed_header;
+
+  struct header *start_header = freed_header;
+  struct footer *end_footer = (struct footer *)((char *)freed_header + HEADER_SIZE + freed_header->payload_size);
+  size_t total_payload_size = freed_header->payload_size;
+
+  struct header *search_header = freed_header;
+  while ((char *)search_header != starting_break && search_header->is_free) {
+    struct footer *prev_footer = (struct footer *)((char *)search_header - FOOTER_SIZE);
+    struct header *prev_header = (struct header *)((char *)prev_footer - prev_footer->payload_size - HEADER_SIZE);
+
+    if (prev_header->is_free) {
+      remove_list_node(prev_header);
+      start_header = prev_header;
+      total_payload_size = total_payload_size + prev_header->payload_size + FOOTER_SIZE + HEADER_SIZE;
+    } 
+    search_header = prev_header;
+  }
+  
+  search_header = freed_header;
+  while ((char *)search_header != sbrk(0) && search_header->is_free) {
+    struct header *next_header = (struct header *)((char *)search_header + HEADER_SIZE + search_header->payload_size + FOOTER_SIZE);
+
+   if ((char *)next_header != sbrk(0) && next_header->is_free) {
+      remove_list_node(next_header);
+      end_footer = (struct footer*)((char *)next_header + HEADER_SIZE + next_header->payload_size);
+      total_payload_size = total_payload_size + FOOTER_SIZE + HEADER_SIZE + next_header->payload_size;
+    }
+    search_header = next_header;
+  }
+
+  start_header->payload_size = end_footer->payload_size = total_payload_size;
+  add_head_node(start_header);
 }
 
